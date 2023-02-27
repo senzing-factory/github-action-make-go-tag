6fbf7cf47cea089399a36dab23852d0b2ba34868
docktermj <michael@dockter.com>
2023-02-27 10:06:57 -0500

    #3 Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,17 @@
 set -x
 set -eu
 
-# Reference: https://github.com/rickstaa/action-create-tag/blob/main/entrypoint.sh
-
 # Apply hotfix for 'fatal: unsafe repository' error.
+
 git config --global --add safe.directory "${GITHUB_WORKSPACE}"
+
+# Required git configuration.
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
+# Make the tag.
+
 cd "${GITHUB_WORKSPACE}" || exit
-
-git checkout main
-export GIT_SHA=$(git rev-list -n 1 ${GITHUB_REF_NAME})
-
-env
-
-git tag -a "v${GITHUB_REF_NAME}" -m "Go module tag for version ${GITHUB_REF_NAME} by ${GITHUB_ACTOR}" ${GIT_SHA}
-git push origin "v${GITHUB_REF_NAME}"
+git tag -a "v${GITHUB_REF_NAME}" -m "Go module tag for version ${GITHUB_REF_NAME} by ${GITHUB_ACTOR}" ${GITHUB_WORKFLOW_SHA}
+git push origin --tags
