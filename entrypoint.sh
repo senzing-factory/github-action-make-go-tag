6a66fc0798b5888a21c30fe602540f338e97ebad
docktermj <michael@dockter.com>
2023-02-27 08:54:15 -0500

    #3 Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,16 @@ set -eu
 
 # Reference: https://github.com/rickstaa/action-create-tag/blob/main/entrypoint.sh
 
-# Apply hotfix for 'fatal: unsafe repository' error (see #10).
+# Apply hotfix for 'fatal: unsafe repository' error.
 git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 
 cd "${GITHUB_WORKSPACE}" || exit
 
+#git checkout main
+#git pull
 export GIT_SHA=$(git rev-list -n 1 ${GIT_TAG})
-git tag -a "v${GIT_TAG}" -m "Go module tag for version ${GIT_TAG}" ${GIT_SHA}
-git push origin --tags
+
+env
+
+# git tag -a "v${GIT_TAG}" -m "Go module tag for version ${GIT_TAG}" ${GIT_SHA}
+# git push origin --tags
